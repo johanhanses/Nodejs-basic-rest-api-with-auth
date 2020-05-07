2f580033b3677e2816095dd10d4e3bca190c69b4
Johan Hanses <johanhanses@gmail.com>
2020-05-07 14:47:11 +0200

    added and implemented cors package

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 require("./db/mongoose");
 const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
@@ -6,6 +7,7 @@ const taskRouter = require("./routers/task");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(userRouter);
 app.use(taskRouter);
 
